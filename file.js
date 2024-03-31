b5c7db06cab20d29edd3984fd1a2728963acca86
avi_singh <avi.v.singh@gmail.com>
2024-03-31 20:12:12 +0100

    fifth commit - second attempt game logic, still some bugs, if repeatedly submitting then system div changes

--- file.js
+++ file.js
@@ -5,18 +5,17 @@ const selections = ['rock', 'paper', 'scissors'];
 let refreshPage = () => location.reload();
 
 // Function to generate a random computer choice
-let computerChoice = () => {
+let generateComputerChoice = () => {
   const randomIndex = Math.floor(Math.random() * selections.length);
   return selections[randomIndex];
 };
-console.log(computerChoice());
+console.log(generateComputerChoice());
 
 // Take in user choice and process versus computer
-function handleUserChoice(computerChoice) {
+function handleUserChoice(computerSelection) {
   let userChoiceInput = document.getElementById('user-choice');
   let userChoice = userChoiceInput.value.trim().toLowerCase();
   let statusMessageElement = document.getElementById('status-message');
-
 
   if (!selections.includes(userChoice)) {
     statusMessageElement.classList.add('alert', 'alert-danger');
@@ -24,14 +23,14 @@ function handleUserChoice(computerChoice) {
     return 'nil';
   }
 
-  if (userChoice === computerChoice) {
+  if (userChoice === computerSelection) {
     // It's a tie
     statusMessageElement.classList.add('alert', 'alert-warning');
     statusMessageElement.innerHTML = 'It\'s a tie!';
   } else if (
-    (userChoice === 'rock' && computerChoice === 'scissors') ||
-    (userChoice === 'paper' && computerChoice === 'rock') ||
-    (userChoice === 'scissors' && computerChoice === 'paper')
+    (userChoice === 'rock' && computerSelection === 'scissors') ||
+    (userChoice === 'paper' && computerSelection === 'rock') ||
+    (userChoice === 'scissors' && computerSelection === 'paper')
   ) {
     // User wins
     statusMessageElement.classList.add('alert', 'alert-success');
@@ -41,5 +40,5 @@ function handleUserChoice(computerChoice) {
     statusMessageElement.classList.add('alert', 'alert-secondary');
     statusMessageElement.innerHTML = 'Computer wins! Better luck next time';
   }
-};
-
+  submitButton.disabled = false; // Re-enable the button
+}
