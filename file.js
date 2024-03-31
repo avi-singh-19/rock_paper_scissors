bb2f266bdf3ae197d9f8bbe4a64a56091bc42845
avi_singh <avi.v.singh@gmail.com>
2024-03-31 18:59:20 +0100

    third commit - tidying user choice validation function

--- file.js
+++ file.js
@@ -13,7 +13,7 @@ let computerChoice = () => {
 // take in user choice and validate it
 function handleUserChoice() {
     let userChoiceInput = document.getElementById('user-choice');
-    let userChoice = userChoiceInput.value.trim();
+    let userChoice = userChoiceInput.value.trim().toLowerCase();
 
     if (!selections.includes(userChoice)) {
         let errorMessageElement = document.getElementById('status-message');
