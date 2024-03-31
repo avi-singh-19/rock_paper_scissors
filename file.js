d03777fdb7725352b36adf11405994dafa8c6d4f
avi_singh <avi.v.singh@gmail.com>
2024-03-31 19:27:02 +0100

    fourth commit - first attempt game logic, still some bugs

--- file.js
+++ file.js
@@ -1,23 +1,45 @@
-// valid selections
+// Valid selections
 const selections = ['rock', 'paper', 'scissors'];
 
-// refresh page button
+// Refresh page button
 let refreshPage = () => location.reload();
 
-// computer choice
+// Function to generate a random computer choice
 let computerChoice = () => {
   const randomIndex = Math.floor(Math.random() * selections.length);
   return selections[randomIndex];
 };
+console.log(computerChoice());
 
-// take in user choice and validate it
-function handleUserChoice() {
-    let userChoiceInput = document.getElementById('user-choice');
-    let userChoice = userChoiceInput.value.trim().toLowerCase();
+// Take in user choice and process versus computer
+function handleUserChoice(computerChoice) {
+  let userChoiceInput = document.getElementById('user-choice');
+  let userChoice = userChoiceInput.value.trim().toLowerCase();
+  let statusMessageElement = document.getElementById('status-message');
 
-    if (!selections.includes(userChoice)) {
-        let errorMessageElement = document.getElementById('status-message');
-        errorMessageElement.classList.add('alert', 'alert-danger');
-        errorMessageElement.innerHTML = 'Error - invalid input';
-    }
+
+  if (!selections.includes(userChoice)) {
+    statusMessageElement.classList.add('alert', 'alert-danger');
+    statusMessageElement.innerHTML = 'Error - invalid input';
+    return 'nil';
+  }
+
+  if (userChoice === computerChoice) {
+    // It's a tie
+    statusMessageElement.classList.add('alert', 'alert-warning');
+    statusMessageElement.innerHTML = 'It\'s a tie!';
+  } else if (
+    (userChoice === 'rock' && computerChoice === 'scissors') ||
+    (userChoice === 'paper' && computerChoice === 'rock') ||
+    (userChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    // User wins
+    statusMessageElement.classList.add('alert', 'alert-success');
+    statusMessageElement.innerHTML = 'Congratulations! You win!';
+  } else {
+    // Computer wins
+    statusMessageElement.classList.add('alert', 'alert-secondary');
+    statusMessageElement.innerHTML = 'Computer wins! Better luck next time';
+  }
 };
+
