50a3a49bf668f95a86d7764ca755efdb2c9193d6
avi_singh <avi.v.singh@gmail.com>
2024-04-02 18:16:48 +0100

    sixth commit - finished game logic, added features to hide submit button after clicking and to display computer choice on screen

--- file.js
+++ file.js
@@ -9,25 +9,32 @@ let generateComputerChoice = () => {
   const randomIndex = Math.floor(Math.random() * selections.length);
   return selections[randomIndex];
 };
-console.log(generateComputerChoice());
 
 // Take in user choice and process versus computer
 function handleUserChoice() {
   let submitButton = document.getElementById('submit-button');
-  submitButton.disabled = true; // Disable the button
-
   let userChoiceInput = document.getElementById('user-choice');
   let userChoice = userChoiceInput.value.trim().toLowerCase();
   let statusMessageElement = document.getElementById('status-message');
+  let computerChoiceElement = document.getElementById('computer-choice');
+  let playAgainButton = document.getElementById('refresh-button');
+
+  submitButton.disabled = true;
+  submitButton.style.display = 'none';
+  playAgainButton.style.display = 'block';
+
 
   if (!selections.includes(userChoice)) {
     statusMessageElement.classList.add('alert', 'alert-danger');
     statusMessageElement.innerHTML = 'Error - invalid input';
-    submitButton.disabled = false; // Re-enable the button
+    submitButton.disabled = false;
+    submitButton.style.display = 'block';
+    playAgainButton.style.display = 'none';
     return 'nil';
   }
 
   let computerChoice = generateComputerChoice();
+  computerChoiceElement.innerHTML = `Computer chose: ${computerChoice}`;
 
   if (userChoice === computerChoice) {
     // It's a tie
@@ -43,9 +50,7 @@ function handleUserChoice() {
     statusMessageElement.innerHTML = 'Congratulations! You win!';
   } else {
     // Computer wins
-    statusMessageElement.classList.add('alert', 'alert-secondary');
+    statusMessageElement.classList.add('alert', 'alert-danger');
     statusMessageElement.innerHTML = 'Computer wins! Better luck next time';
   }
-
-  submitButton.disabled = false; // Re-enable the button
 }
